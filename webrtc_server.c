6f782f9fca5925c2e2524b75f692303219f9ff31
TracopkZ TP <tracopk.wb@gmail.com>
2026-08-16 02:07:28 -0600

    PLEASEEEEEEE

--- webrtc_server.c
+++ webrtc_server.c
@@ -224,10 +224,11 @@ static void on_ws_opened(SoupServer *server, SoupServerMessage *msg, const char 
     gchar *pipeline_str = g_strdup_printf(
         "webrtcbin name=sendrecv stun-server=stun://stun.l.google.com:19302 "
         "rtspsrc name=rtspsrc location=\"rtsp://10.0.0.210:554/Streaming/Channels/102\" "
-        "user-id=\"bigEye\" user-pw=\"traHiLook1\" protocols=udp latency=200 ! "
+        "user-id=\"bigEye\" user-pw=\"traHiLook1\" protocols=tcp latency=200 ! "
         "rtph264depay name=depay ! h264parse ! avdec_h264 ! "
-        "videoconvert ! x264enc speed-preset=ultrafast tune=zerolatency key-int-max=15 ! "
-        "video/x-h264,profile=baseline ! "
+        "videoconvert ! video/x-raw,format=I420 ! "
+        "x264enc speed-preset=ultrafast tune=zerolatency key-int-max=15 ! "
+        "video/x-h264,profile=baseline,stream-format=byte-stream,alignment=au ! "
         "rtph264pay config-interval=1 pt=96 aggregate-mode=zero-latency ! "
         "application/x-rtp,media=video,clock-rate=90000,encoding-name=H264,payload=96 ! "
         "queue name=videoqueue");
